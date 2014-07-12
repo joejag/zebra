403d04f7ec87804b4a387b1216e1b60a9d3af251
Joe Wright <joe@joejag.com>
2014-07-12 18:41:46 +0100

    BruteForcer: go back

--- src/zebra/brute_forcer.clj
+++ src/zebra/brute_forcer.clj
@@ -1,8 +1,37 @@
 (ns zebra.brute-forcer)
 
-(defn- find-first-key-with-nil-value [map]
+(defn- find-next-updateable-value [map]
   (first (first (filter (fn [[_ v]] (nil? v)) map))))
 
+(defn- find-next-possiblity [current possibilities]
+  (let [index (.indexOf possibilities current)]
+    (get possibilities (inc index))))
+
 (defn next-possibility [state possibilities]
-  (let [item-to-change (find-first-key-with-nil-value state)]
+  (let [item-to-change (find-next-updateable-value state)]
     (assoc state item-to-change (first (item-to-change possibilities)))))
+
+
+
+
+(defn- available-to-update? [[k v] possibities]
+  (and (not (nil? v)) (not= v (last (k possibities)))))
+
+(defn- find-last-updateable-value [state possibities]
+  (last (filter (fn [v] (available-to-update? v possibities)) state)))
+
+(defn- items-after-element [item coll]
+  (subvec coll (.indexOf coll item)))
+
+(defn- keys-list [coll]
+  (vec (map (fn [[k _]] k) coll)))
+
+(defn update-vals [map vals f]
+  (reduce #(update-in % [%2] f) map vals))
+
+(defn change-answer [state possibilities]
+  (let [[last-key last-value] (find-last-updateable-value state possibilities)
+        next-value (find-next-possiblity last-value (last-key possibilities))
+        keys-to-blank-out (items-after-element last-key (keys-list state))
+        cleared-state (update-vals state keys-to-blank-out (fn[_] nil))]
+    (assoc cleared-state last-key next-value)))
--- test/zebra/brute_forcer_test.clj
+++ test/zebra/brute_forcer_test.clj
@@ -11,3 +11,10 @@
        (fact "it suggests the first possibilty possible for later keys"
              (next-possibility (sorted-map :a 1 :b nil) possibilities) => {:a 1 :b 4}))
 
+(facts "handle changing the answer"
+       (fact "it can choose the next option"
+             (change-answer (sorted-map :a 1 :b 4) possibilities) => {:a 1 :b 5})
+       (fact "it can choose the next option again"
+             (change-answer (sorted-map :a 1 :b 5) possibilities) => {:a 1 :b 6})
+       (fact "it can backtrack then the possibilties are exhusted"
+             (change-answer (sorted-map :a 1 :b 6) possibilities) => {:a 2 :b nil}))
